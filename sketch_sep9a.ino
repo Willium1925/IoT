a82d15ae2767466ffd468214aa9b28a41b19ddad
JIN <d1397026@o365.fcu.edu.tw>
2025-09-09 11:34:01 +0800

    Initial commit

---
+++ sketch_sep9a.ino
@@ -0,0 +1,23 @@
+void setup() {
+  // put your setup code here, to run once: 
+  // 初始化區塊
+  // 通訊相關，pin就是腳
+  // 使用內建的led的那隻腳
+  // 想像我是板子，所以我要發訊號給led燈，安安
+  pinMode(LED_BUILTIN, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // 用數位訊號的形式
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+
+}
